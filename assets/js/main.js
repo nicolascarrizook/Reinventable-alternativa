a45862258a1fea275c278170707c0ad8e043824a
nicolascarrizook <nicolascarrizook@gmail.com>
2021-09-14 01:34:11 -0300

    Se suben los demas archivos

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+const imgBx = document.querySelector('.imgBx');
+const contentBx = document.querySelector('.contentBx');
+const slides = imgBx.getElementsByTagName('img');
+const slidesText = contentBx.getElementsByTagName('div');
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+var i = 0;
+var j = 0;
+
+prev.addEventListener('click', function(){
+	anteriorSlide();
+	anteriorSlideText();
+});
+next.addEventListener('click', function(){
+	proximoSlide();
+	proximoSlideText();
+});
+
+function proximoSlide(){
+	slides[i].classList.remove('active');
+	i = (i + 1) % slides.length;
+	slides[i].classList.add('active');
+}
+
+function anteriorSlide(){
+	slides[i].classList.remove('active');
+	i = (i - 1 + slides.length) % slides.length;
+	slides[i].classList.add('active');
+}
+
+function proximoSlideText(){
+	slidesText[j].classList.remove('active');
+	j = (j + 1) % slidesText.length;
+	slidesText[j].classList.add('active');
+}
+
+function anteriorSlideText(){
+	slidesText[j].classList.remove('active');
+	j = (j - 1 + slidesText.length) % slidesText.length;
+	slidesText[j].classList.add('active');
+}
